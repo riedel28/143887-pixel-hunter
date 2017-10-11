134a3fc777a29209fc6cf7728745d054f87022c6
Sergey <sergey.riedel@gmail.com>
2017-10-11 17:22:17 +0400

    Написал функцию подсчета очков

--- js/utils.js
+++ js/utils.js
@@ -24,28 +24,61 @@ const renderScreen = (screen) => {
 
 export {renderScreen};
 
-const getTotalScore = (answers) => {
-  const correctAnswer = 100;
-  const slowAnswer = 50;
-  const fastAnswer = 150;
+const isAnswerCorrect = (answer) => {
+  // const correctAnswer = 100;
+  // const slowAnswer = 50;
+  // const fastAnswer = 150;
 
+  if (answer === `correct`) {
+    return 100;
+  } else if (answer === `fast`) {
+    return 150;
+  } else if (answer === `slow`) {
+    return 50;
+  } else {
+    return 0;
+  }
+};
+
+export {isAnswerCorrect};
+
+const getTotalScore = (answers, lives) => {
   if (answers.length < 10) {
     return -1;
   }
 
   let totalScore = 0;
 
-  for (const answer of answers) {
-    if (answer === `correct`) {
-      totalScore += correctAnswer;
-    } else if (answer === `fast`) {
-      totalScore += fastAnswer;
-    } else {
-      totalScore += slowAnswer;
-    }
-  }
+  answers.forEach((answer) => {
+    isAnswerCorrect(answer);
+  });
+
+  totalScore += lives * 50;
 
   return totalScore;
 };
 
 export {getTotalScore};
+
+const getTime = (time) => {
+  // if (time >= 0 && time < 10) {
+  //   return `fast`;
+  // } else if (time >= 10 && time <= 20) {
+  //   return `ok`;
+  // } else if (time > 20 && time <= 30) {
+  //   return `slow`;
+  // } else {
+  //   return `error`;
+  // }
+  if (time === 0) {
+    return `Время истекло`;
+  }
+
+  return {
+    tick() {
+      return getTime(time - 1);
+    }
+  };
+};
+
+export {getTime};
