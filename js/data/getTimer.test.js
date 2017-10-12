f0396d4a1d9dc78a6daca96714b871cc1f322506
Sergey <sergey.riedel@gmail.com>
2017-10-12 22:28:42 +0400

    Исправил тесты для таймера

--- js/data/getTimer.test.js
+++ js/data/getTimer.test.js
@@ -4,28 +4,28 @@ import {getTimer} from "./../utils";
 describe(`test`, () => {
   it(`should return 0`, () => {
     const timer = getTimer(5);
-    let tick = 0;
-    for (let i = 5; i < 5; i--) {
-      tick = timer.tick();
+
+    for (let i = 0; i < 5; i++) {
+      timer.tick();
     }
-    assert.equal(tick, 0);
+    assert.equal(timer.getCurrentTimer(), 0);
   });
 
-  it(`should return 10`, () => {
+  it(`should return 5`, () => {
     const timer = getTimer(10);
-    let tick = 0;
-    for (let i = 10; i < 10; i--) {
-      tick = timer.tick();
+
+    for (let i = 0; i < 5; i++) {
+      timer.tick();
     }
-    assert.equal(tick, 0);
+    assert.equal(timer.getCurrentTimer(), 5);
   });
 
   it(`should return 15`, () => {
-    const timer = getTimer(15);
-    let tick = 0;
-    for (let i = 15; i < 15; i--) {
-      tick = timer.tick();
+    const timer = getTimer(20);
+
+    for (let i = 0; i < 10; i++) {
+      timer.tick();
     }
-    assert.equal(tick, 0);
+    assert.equal(timer.getCurrentTimer(), 10);
   });
 });
