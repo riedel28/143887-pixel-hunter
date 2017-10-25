0149f236b7b4deb0b083d0d735481e5dd4a94df9
Sergey <sergey.riedel@gmail.com>
2017-10-25 19:41:26 +0400

    Добавлен показ экрана Stats после показа всех игровыъ экранов

--- js/Game.js
+++ js/Game.js
@@ -4,14 +4,14 @@ import game3 from "./Game-3";
 import stats from "./Stats";
 import {renderScreen} from "./utils";
 import getNextScreenData from "./data/state";
-import state from "./data/state";
+// import state from "./data/state";
 
 const games = [game1, game2, game3];
 
 const getNextScreen = () => {
   const nextScreenData = getNextScreenData();
 
-  if (state.currentScreen === 10) {
+  if (!nextScreenData) {
     renderScreen(stats);
   }
 
