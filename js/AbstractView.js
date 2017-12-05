5ca5a45bc860fcba8d25ad05de6b74b77351f0b7
Sergey <sergey.riedel@gmail.com>
2017-12-05 15:59:46 +0100

    Создал класс AbstractView

---
+++ js/AbstractView.js
@@ -0,0 +1,22 @@
+import {getElementFromTemplate} from "./utils";
+
+export default class AbstractView {
+  get template() {
+    throw new Error(`You have to define template for view`);
+  }
+
+  render() {
+    return getElementFromTemplate(this.template);
+  }
+
+  bindHandlers() {}
+
+  get element() {
+    if (!this.element) {
+      this._element = this.render();
+      this.bindHandlers();
+    }
+
+    return this._element;
+  }
+}
